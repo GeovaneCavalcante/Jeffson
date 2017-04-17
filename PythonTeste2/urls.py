35471dd1413f5b5b6d4116ed7dd497afe4b3abb6
GeovaneDevelop <geovanefeitosacavalcante@gmail.com/>
2017-04-17 10:38:34 -0300

    novo

--- PythonTeste2/urls.py
+++ PythonTeste2/urls.py
@@ -6,6 +6,10 @@ from datetime import datetime
 from django.conf.urls import url
 import django.contrib.auth.views
 
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
 import app.forms
 import app.views
 
@@ -74,4 +78,4 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
